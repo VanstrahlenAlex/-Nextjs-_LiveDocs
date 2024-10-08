bf0d0f5343f025fb3ab37f2966860c561037544e
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2024-10-08 16:03:30 -0500

    8-10-2024

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -2,8 +2,14 @@
 import React from 'react'
 import { ClientSideSuspense, LiveblocksProvider } from '@liveblocks/react/suspense';
 import Loader from '@/components/Loader';
+import { currentUser } from '@clerk/nextjs/server';
+import { useUser } from '@clerk/nextjs';
+import { getDocumentUsers } from '@/lib/actions/user.actions';
 
 const Provider = ({ children } : { children: React.ReactNode }) => {
+
+	const { user: clerkUser } = useUser();
+
 	function getClerkUser() {
 		throw new Error('Function not implemented.');
 	}
@@ -19,6 +25,14 @@ const Provider = ({ children } : { children: React.ReactNode }) => {
 				resolveUsers={async({userIds}) => {
 					const users = await getClerkUser({ userIds});
 					return users;	
+				}}
+				resolveMentionSuggestions={async({ text, roomId}) => {
+					const roomUsers = await getDocumentUsers({ 
+						roomId,
+						currentUser: clerkUser?.emailAddresses[0].emailAddress!,
+						text,
+					});
+					return roomUsers;
 				}}
 			>
 					<ClientSideSuspense fallback={<Loader />}>
